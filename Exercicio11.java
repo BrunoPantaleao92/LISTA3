e48dba6aff7aa2dc31e4831e9efd37d60ed54fc2
BrunoPantaleao92 <bruno_92k@hotmail.com>
2024-03-31 23:00:30 -0300

    Exercicio 11

--- Exercicio11.java
+++ Exercicio11.java
@@ -6,28 +6,8 @@ public class Exercicio11 {
          * e se ela é homem ou mulher. No final informe total de homens e de mulheres.
          */
         Pessoa11 pessoa = new Pessoa11();
-        int homens = 0;
-        int mulheres = 0;
 
-        for (int i = 0; i < 3; i++) {
-            pessoa.nome = Prompt.lerLinha("Digite o nome: ");
-            pessoa.sexo = Prompt.lerLinha("Digite o sexo (M / H): ");
-
-            if (pessoa.sexo == "M" || pessoa.sexo == "m") {
-                Prompt.imprimir(pessoa.nome);
-                Prompt.imprimir("Mulher");
-                mulheres++;
-            }
-
-            if (pessoa.sexo == "H" || pessoa.sexo == "h") {
-                Prompt.imprimir(pessoa.nome);
-                Prompt.imprimir("Homem");
-                homens++;
-            }
-        }
-
-        Prompt.imprimir("Total homens: " + homens + "\n");
-        Prompt.imprimir("Total mulheres: " + mulheres);
+        System.out.println(pessoa.contarSexo());
 
     }
 }
